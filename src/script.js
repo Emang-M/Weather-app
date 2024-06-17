c5a9657105dfd5dc18dc1e99174370f8dd48135c
Emang-M <emangmachola@gmail.com>
2024-06-17 23:10:23 +0200

    Update script.js
    script still not working

--- src/script.js
+++ src/script.js
@@ -1,31 +1,25 @@
-function currentWeather(response) {
-  console.log(response);
-  let temperatureElement= document.querySelector("#temperature");
-  let temperature=response.data.temperature.current;
-    temperatureElement.innerHTML=Math.round(temperature);
+function refreshWeather(response) {
+console.log(response);
+let temperatureElement= document.querySelector("#temperature");
+let temperature=response.data.temperature.current;
+let cityElement= document.querySelector("#city");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windSpeedElement=document.querySelector("#wind-speed");
+let timeElement=document.querySelector("#time");
+let date=new Date(response.data.time * 1000);
+let iconElement= document.querySelector("#icon");
 
-  let cityElement= document.querySelector("#city");
-    cityElement.innerHTML= response.data.city;
-
-     let descriptionElement=document.querySelector("#description");
-    descriptionElement.innerHTML= response.data.condition.description;
-
-  let humidityElement=document.querySelector("#humidity");
-    humidityElement.innerHTML= '${response.data.temperature.humidity}%';
-
-  let windSpeedElement=document.querySelector("#wind-speed");
-    windSpeedElement.innerHTML='${response.data.wind.speed}km/h';
-
-  let timeElement=document.querySelector("#time");
-  let timeStamp=response.data.time * 1000;
-  let date= new Date(timeStamp);
-    timeElement.innerHTML= currentDate(date);
-
-   let iconElement= document.querySelector("#icon");
+cityElement.innerHTML= response.data.city;
+timeElement.innerHTML= formatDate(date);
+descriptionElement.innerHTML= response.data.condition.description;
+humidityElement.innerHTML= '${response.data.temperature.humidity}%';
+windSpeedElement.innerHTML= '${response.data.wind.speed}km/h';
+temperatureElement.innerHTML=Math.round(temperature);
 iconElement.innerHTML='<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>';
 }
 
-function currentDate(date) {
+function formatDate(date) {
   let minutes= date.getMinutes();
   let hours= date.getHours();
   
@@ -50,17 +44,19 @@ function currentDate(date) {
   return '${day} {$hours}:${minutes}';
 }
 
-
+function searchCity(city){
+let apiKey="b2a5adcct04b33178913oc335f405433";
+    let apiUrl='https://api.shecodes.io/weather/v1/current?query=${searchCity}&key=${apiKey}&units=metric';
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput= document.querySelector("#search-form-input");
-  let searchCity= searchInput.value;
-  let apiKey="b2a5adcct04b33178913oc335f405433";
-    let apiUrl='https://api.shecodes.io/weather/v1/current?query=${searchCity}&key=${apiKey}&units=metric';
-    axios.get(apiUrl).then(currentWeather);
-}
+  searchCity= searchInput.value;
+  }
 
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
-searchSubmit("Kasane");
+searchCity("Kasane");
